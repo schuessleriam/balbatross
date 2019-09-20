4c010ecba2fadde9e017dba3716960acc771db01
Alex Schuessler <AS@schuess.com>
2019-09-20 08:04:47 -0500

    converted sign-in and sign-up components to functional components using hooks

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './sign-in.styles.scss';
 import FormInput from './../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -7,71 +7,66 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectUserErrorMessage } from './../../redux/user/user.selectors';
 
-class SignIn extends Component {
-    constructor(props){
-        super(props);
+const SignIn = ({ 
+    emailSignInStart, 
+    SignInWithGoogle, 
+    userErrorMessage, 
+    clearError }) => {
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
+    const [userCredentials, setUserCredentials] = useState({email: '', password: ''});
+    const {email, password} = userCredentials;
 
-    handleSubmit = async e => {
+    const handleSubmit = async e => {
         e.preventDefault();
-        const {email, password} = this.state;
-        const {emailSignInStart} = this.props;
-
         emailSignInStart(email, password);
     }
 
-    handleChange = e => {
+    const handleChange = e => {
         const { value, name } = e.target;
-
-        this.setState({ [name]: value})
+        setUserCredentials({...userCredentials, [name]: value});
     }
 
-    render(){
-
-        const { SignInWithGoogle, userErrorMessage, clearError } = this.props;
-
+    useEffect(() => {
         if(userErrorMessage){
             alert(userErrorMessage);
             clearError();
+            setUserCredentials({...userCredentials, password: ''});
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[userErrorMessage]); //warning shown as result of not including 
+                           //clearError and SetUserCredentials, no real problem.
 
-        return(
-            <div className='sign-in'>
-                <h2 className="title">I already have an account</h2>
-                <span>Sign in with username and password</span>
+    return(
+        <div className='sign-in'>
+            <h2 className="title">I already have an account</h2>
+            <span>Sign in with username and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email' 
-                        type='email' 
-                        value={this.state.email} 
-                        handleChange={this.handleChange}
-                        label='email'
-                        required/>
-                    <FormInput name='password' 
-                        type='password' 
-                        value={this.state.password} 
-                        handleChange={this.handleChange}
-                        label='Password'
-                        required/>
-                    <div className="buttons">
-                        <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton type='button' isGoogle='true' onClick={
-                            SignInWithGoogle 
-                        }>
-                        Sign In With Google
-                        </CustomButton>
-                    </div>    
-                </form>
-            </div>
-        );
-    }
-
+            <form onSubmit={handleSubmit}>
+                <FormInput name='email' 
+                    type='email' 
+                    value={email} 
+                    handleChange={handleChange}
+                    label='email'
+                    required/>
+                <FormInput name='password' 
+                    type='password' 
+                    value={password} 
+                    handleChange={handleChange}
+                    label='Password'
+                    required/>
+                <div className="buttons">
+                    <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton type='button' isGoogle='true' onClick={
+                        SignInWithGoogle 
+                    }>
+                    Sign In With Google
+                    </CustomButton>
+                </div>    
+            </form>
+        </div>
+    );
 }
+
 
 const mapStateToProps = createStructuredSelector({
     userErrorMessage: selectUserErrorMessage
--- src/pages/account/account.component.jsx
+++ src/pages/account/account.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './account.styles.scss';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
@@ -6,10 +6,24 @@ import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './../../redux/user/user.selectors.js';
 import CustomButton from './../../components/custom-button/custom-button.component';
 
-const AccountPage = ({user: {displayName, email}, history}) => (
+const AccountPage = ({user: {displayName, email}, history}) => {
+
+    const [firstNameState, setFirstName] = useState({firstName: ''})
+    const {firstName} = firstNameState;
+    useEffect(() => {
+        const slicedName = displayName.substring(0,displayName.indexOf(" "));
+        if (slicedName){
+            setFirstName({firstName: slicedName});
+        }else{
+            setFirstName({firstName: displayName})
+        }
+    }, [displayName]);
+    
+
+    return(
     <div className='account-page-container'>
         <h1 className='title'>
-            Welcome, {displayName.substring(0,displayName.indexOf(" "))}
+            Welcome, {firstName}
         </h1> 
         <span className='email'>
             Email: {email} 
@@ -17,7 +31,8 @@ const AccountPage = ({user: {displayName, email}, history}) => (
         <CustomButton onClick={() => history.push('/shop')}>Go To Shop</CustomButton>
         <CustomButton inverted onClick={() => history.push('/checkout')}>Go To Cart</CustomButton>
     </div>
-);
+    );
+}
 
 const mapStateToProps = createStructuredSelector({
     user: selectCurrentUser
